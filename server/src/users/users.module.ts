2ef339588622e6be4923d1798df7fda28a8b247d
Jason <jiacan.h01@gmail.com>
2023-05-31 19:47:54 -0400

    finally finished set up

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
 @Module({
   providers: [UsersService],
-  controllers: [UsersController]
 })
 export class UsersModule {}
