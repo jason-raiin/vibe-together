f42e8b309131720477da6227453a865dbc1c2a9a
Jason <jiacan.h01@gmail.com>
2023-06-30 20:51:16 +0800

    converted a constants file to js

--- client/src/components/constants.js
+++ client/src/components/constants.js
@@ -1,7 +1,6 @@
 import { CLIENT_ID, SPOTIFY_LOGIN_URL } from '../spotify/constants';
 
 const APP_URI = process.env.REACT_APP_URL;
-
 export const REDIRECT_URI = `${APP_URI}/callback`;
 export const LOGIN_URI = `${SPOTIFY_LOGIN_URL}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=user-top-read`;
 
