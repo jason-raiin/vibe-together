8b5db46d2069366b213619b88e34cdb7e62a7be9
Jason <jiacan.h01@gmail.com>
2023-07-06 17:01:05 +0800

    hotfix for deployment issues

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const logger = new Logger('Nest');
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
@@ -13,20 +11,6 @@ async function bootstrap() {
 
   app.enableCors({ origin: CLIENT_URL });
   await app.listen(PORT);
-
-  const server = app.getHttpServer();
-  const router = server._events.request._router;
-  const routes = router.stack
-    .map((layer) => {
-      if (layer.route) {
-        const path = layer.route?.path;
-        const method = layer.route?.stack[0].method.toUpperCase();
-        return `${method} ${path}`;
-      }
-    })
-    .filter((route: string) => route);
-
-  logger.log(routes);
 }
 
 bootstrap();
