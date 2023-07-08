a00368a12a02e2196ba7ffe8c4ae252319267b89
Jason <jiacan.h01@gmail.com>
2023-07-08 15:03:00 +0800

    updating npm packages

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Nest');
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
@@ -11,6 +13,20 @@ async function bootstrap() {
 
   app.enableCors({ origin: CLIENT_URL });
   await app.listen(PORT);
+
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+  const routes = router.stack
+    .map((layer) => {
+      if (layer.route) {
+        const path = layer.route?.path;
+        const method = layer.route?.stack[0].method.toUpperCase();
+        return `${method} ${path}`;
+      }
+    })
+    .filter((route: string) => route);
+
+  logger.log(routes);
 }
 
 bootstrap();
