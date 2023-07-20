1eb7360489ed7b441b1c8bf32ffc58728761c769
Jason <jiacan.h01@gmail.com>
2023-07-20 15:35:24 +0800

    attempt at adding images (WIP)

--- client/src/components/venn.js
+++ client/src/components/venn.js
@@ -4,6 +4,9 @@ import * as d3 from 'd3';
 import './venn.css';
 
 const FONT = { family: 'sans-serif', size: '12px' };
+const COLORS = ['palegoldenrod', 'paleturquoise', 'palevioletred'];
+const DEFAULT_OPACITY = 0.4;
+
 export default function VennDiagram() {
   useEffect(() => vennChart());
 
@@ -12,19 +15,46 @@ export default function VennDiagram() {
 
 const vennChart = () => {
   const sets = [
-    { sets: ['A'], size: 10 },
-    { sets: ['B'], size: 10 },
-    { sets: ['C'], size: 10 },
-    { sets: ['A', 'B'], size: 2 },
-    { sets: ['A', 'C'], size: 2 },
-    { sets: ['B', 'C'], size: 2 },
-    { sets: ['A', 'B', 'C'], size: 2 },
+    { sets: ['A'], size: 2 },
+    { sets: ['B'], size: 2 },
+    { sets: ['C'], size: 3 },
+    { sets: ['D'], size: 0.2 },
+    { sets: ['A', 'B'], size: 1 },
+    { sets: ['A', 'C'], size: 1 },
+    { sets: ['B', 'C'], size: 1 },
+    { sets: ['C', 'D'], size: 1 },
+    { sets: ['A', 'B', 'C'], size: 1 },
   ];
 
-  // draw
+  // draw default venn diagram
   const chart = venn.VennDiagram();
   const div = d3.select('#venn');
   div.datum(sets).call(chart);
+
+  // circle styles
+  d3.selectAll('#venn .venn-circle path')
+    .style('fill', (_, i) => (i < COLORS.length ? COLORS[i] : 'grey'))
+    .style('fill-opacity', DEFAULT_OPACITY)
+    .style('stroke', (_, i) => COLORS[i])
+    .style('stroke-opacity', 0);
+
+  // TODO: user images
+  // d3.selectAll('#venn .venn-circle path')
+  //   .append('svg:image')
+  //   .attr('x', 0)
+  //   .attr('y', 0)
+  //   .attr('width', 50)
+  //   .attr('height', 50)
+  //   .attr(
+  //     'xlink:href',
+  //     'http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainamerica.png',
+  //   );
+
+  // text label styles
+  d3.selectAll('#venn .venn-circle text')
+    .style('fill', 'black')
+    .style('font-size', '24px')
+    .style('font-weight', '50');
 
   // add a tooltip
   const tooltip = d3
@@ -47,11 +77,11 @@ const vennChart = () => {
       tooltip.text(d.size + ' users').style('visibility', 'visible');
 
       // highlight the current path
-      const selection = d3.select(this).transition('tooltip').duration(400);
+      const selection = d3.select(this).transition('tooltip').duration(200);
       selection
         .select('path')
         .style('stroke-width', 3)
-        .style('fill-opacity', d.sets.length == 1 ? 0.4 : 0.1)
+        .style('fill-opacity', d.sets.length == 1 ? 0.7 : 0.1)
         .style('stroke-opacity', 1);
     })
 
@@ -63,11 +93,11 @@ const vennChart = () => {
 
     .on('mouseout', function (event, d) {
       tooltip.style('visibility', 'hidden');
-      const selection = d3.select(this).transition('tooltip').duration(400);
+      const selection = d3.select(this).transition('tooltip').duration(200);
       selection
         .select('path')
         .style('stroke-width', 0)
-        .style('fill-opacity', d.sets.length == 1 ? 0.25 : 0.0)
+        .style('fill-opacity', d.sets.length == 1 ? DEFAULT_OPACITY : 0.0)
         .style('stroke-opacity', 0);
     });
 };
